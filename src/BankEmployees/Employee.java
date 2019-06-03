e8f14b80f0f98befc1fe29ad45f58e988508bd0f
Daniel A <dalamo90@gmail.com>
2019-06-03 13:23:38 -0500

    Created Employees package that contains class file (Employee and Employees) where 5 employees were created.

---
+++ src/BankEmployees/Employee.java
@@ -0,0 +1,91 @@
+package BankEmployees;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private String department;
+    private String position;
+    private int employeeId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public void setSalary(String salary) {
+        this.salary = Integer.parseInt(salary);
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = Integer.parseInt(employeeId);
+    }
+
+    public Employee(String firstName, String lastName, int salary, String department, String position, int employeeId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.department = department;
+        this.position = position;
+        this.employeeId = employeeId;
+    }
+
+    public Employee(String firstName, String lastName, int salary, int employeeId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.department = "";
+        this.position = "";
+        this.employeeId = employeeId;
+    }
+
+    public Employee() {
+        this.firstName = "";
+        this.lastName = "";
+        this.salary = 0;
+        this.department = "";
+        this.position = "";
+        this.employeeId = 0;
+    }
+}
---
+++ src/BankEmployees/Employees.java
@@ -0,0 +1,20 @@
+package BankEmployees;
+
+public class Employees {
+    public static void main(String[] args){
+
+        Employee employee1 = new Employee("Miguel", "Barnes", 30000, "Finance", "Teller", 923003);
+        Employee employee2 = new Employee("Janet", "Brown", 60000, "Finance", "Loan Officer", 338405);
+        Employee employee3 = new Employee("Amy", "Lee", 45000, "Finance", "Manager", 987123);
+        Employee employee4 = new Employee("Jason", "Ford", 30000, "Finance", "Teller", 930594);
+        Employee employee5 = new Employee("Shashi", "Aziz", 120000, "Finance", "CEO", 123456);
+
+
+        System.out.println("First Name:\t\t" + "Last Name:\t\t" +  "ID Number:\t\t" + "Department:\t\t" + "Position:\t\t" + "Salary:");
+        System.out.println(employee1.getFirstName() + "\t\t\t" + employee1.getLastName() + "\t\t\t" + employee1.getEmployeeId() + "\t\t\t" + employee1.getDepartment() + "\t\t\t" + employee1.getPosition() + "\t\t\t" + employee1.getSalary());
+        System.out.println(employee2.getFirstName() + "\t\t\t" + employee2.getLastName() + "\t\t\t" + employee2.getEmployeeId() + "\t\t\t" + employee2.getDepartment() + "\t\t\t" + employee2.getPosition() + "\t" + employee2.getSalary());
+        System.out.println(employee3.getFirstName() + "\t\t\t\t" + employee3.getLastName() + "\t\t\t\t" + employee3.getEmployeeId() + "\t\t\t" + employee3.getDepartment() + "\t\t\t" + employee3.getPosition() + "\t\t\t" + employee3.getSalary());
+        System.out.println(employee4.getFirstName() + "\t\t\t" + employee4.getLastName() + "\t\t\t" + employee4.getEmployeeId() + "\t\t\t" + employee4.getDepartment() + "\t\t\t" + employee4.getPosition() + "\t\t\t" + employee4.getSalary());
+        System.out.println(employee5.getFirstName() + "\t\t\t" + employee5.getLastName() + "\t\t\t" + employee5.getEmployeeId() + "\t\t\t" + employee5.getDepartment() + "\t\t\t" + employee5.getPosition() + "\t\t\t\t" + employee5.getSalary());
+    }
+}
